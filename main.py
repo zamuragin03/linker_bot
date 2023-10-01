55c9cf499d36ef7dec5272717d453888c27e0ab0
zamuragin03 <zamuragin03@gmail.com>
2023-10-01 16:01:52 +0300

    fix logging

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename="debug.log", filemode="w", level=logging.DEBUG, encoding='UTF-8', format='%(asctime)s - %(levelname)s - %(message)s', )
+logging.basicConfig(filename="debug.log", filemode="w", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', )
 
 scheduler = AsyncIOScheduler()
 
